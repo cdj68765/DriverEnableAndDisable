38b9b1e83cd46c82dd1cb6637f0047287aa79b06
Emil <Emil@EMIL-TABLET>
2018-01-06 00:10:19 +0800

    Star

---
+++ 驱动禁用/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace 驱动禁用
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guid mouseGuid = new Guid("{5175d334-c371-4806-b3ba-71fd53c9258d}");
+         
+            // get this from the properties dialog box of this device in Device Manager
+            string instancePath = @"HID\INV6050\4&418B572&0&0000";
+            DeviceHelper.SetDeviceEnabled(mouseGuid, instancePath, false);
+            Thread.Sleep(1000);
+            DeviceHelper.SetDeviceEnabled(mouseGuid, instancePath, true);
+        }
+    }
+}
